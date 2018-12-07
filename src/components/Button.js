311b587d2c220bf5d4b6919750b995711fde6d1f
Oleh Makarov <gstancox@gmail.com>
2018-12-07 14:56:59 +0200

    converted to tsx , need knobs appear

--- src/components/Button.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-
-export const Button = ({bgColor, children}) => <button style={{background: bgColor}}>{children}</button>
--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -4,6 +4,7 @@ import { storiesOf } from '@storybook/react';
 import { Button } from './Button';
 import { wInfo } from '../utils';
 import { withInfo } from '@storybook/addon-info';
+import { text, boolean } from '@storybook/addon-knobs/react';
 
 storiesOf('Button', module)
 .addWithJSX('with yellow background', wInfo(`
@@ -13,4 +14,8 @@ description
 <Button>slkdjslkdj</Button>
 ~~~
 `)(() => <Button bgColor="yellow">Hello World</Button>))
-.addWithJSX('with green background', withInfo()(() => <Button bgColor="green">Hello World</Button>))
+.addWithJSX('with green background', withInfo()(() =>
+  <Button disabled={boolean('disabled', false)}>
+    {text('text', 'Hello world')}
+  </Button>
+))
